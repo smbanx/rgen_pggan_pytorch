065deaf50643e3b981c3fbce95acc4a51dab7dc9
smbanx <smbanx@gmail.com>
2024-09-26 15:18:02 -0700

    minor changes

--- trainer.py
+++ trainer.py
@@ -266,8 +266,8 @@ class trainer:
                
                 self.fx = self.D(self.x)
                 self.fx_tilde = self.D(self.x_tilde.detach())
-                
-		        loss_d = self.mse(self.fx.squeeze(), self.real_label) + \
+
+                loss_d = self.mse(self.fx.squeeze(), self.real_label) + \
                                   self.mse(self.fx_tilde, self.fake_label)
                 loss_d.backward()
                 self.opt_d.step()
